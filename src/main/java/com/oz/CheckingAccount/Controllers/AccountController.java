b3c0108426a3b317282cd3aceb70581371eb3454
Osmani-Josue Hernandez <osmani.j.hernandez@gmail.com>
2025-02-03 16:56:21 -0500

    don't need this right now. cleanup

--- src/main/java/com/oz/CheckingAccount/Controllers/AccountController.java
+++ src/main/java/com/oz/CheckingAccount/Controllers/AccountController.java
@@ -2,7 +2,6 @@ package com.oz.CheckingAccount.Controllers;
 
 import com.oz.CheckingAccount.Accounts.Account;
 import com.oz.CheckingAccount.Accounts.AccountRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
