b91decf24777ef2de9c56cf0d9f2c2babab802d7
Osmani-Josue Hernandez <osmani.j.hernandez@gmail.com>
2025-02-03 14:50:30 -0500

    initial commit of project

---
+++ src/test/java/com/oz/CheckingAccount/CheckingAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oz.CheckingAccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckingAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
