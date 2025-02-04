89ccd639a847b14afd209454dc1d164c74f47893
Osmani-Josue Hernandez <osmani.j.hernandez@gmail.com>
2025-02-03 21:50:27 -0500

    add test for getting an account

--- src/test/java/com/oz/CheckingAccount/CheckingAccountApplicationTests.java
+++ src/test/java/com/oz/CheckingAccount/CheckingAccountApplicationTests.java
@@ -1,5 +1,7 @@
 package com.oz.CheckingAccount;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 import com.oz.CheckingAccount.Accounts.Account;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,5 +26,25 @@ class CheckingAccountApplicationTests {
 		Account account = new Account("Mary", BigDecimal.valueOf(100.00));
 		ResponseEntity<Void> response = restTemplate.postForEntity("/accounts", account, Void.class);
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+	}
+
+	@Test
+	void getAccount() {
+		Account account = new Account("oz", BigDecimal.valueOf(350.99));
+		ResponseEntity<Void> response = restTemplate.postForEntity("/accounts", account, Void.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+		ResponseEntity<String> responseAccount = restTemplate.getForEntity("/accounts/oz", String.class);
+		assertThat(responseAccount.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		DocumentContext doc = JsonPath.parse(responseAccount.getBody());
+		Number id = doc.read("$.id");
+		assertThat(id).isNotNull();
+
+		String name = doc.read("$.name");
+		assertThat(name).isEqualTo("oz");
+
+		Double balance = doc.read("$.balance");
+		assertThat(balance).isEqualTo(350.99);
 	}
 }
