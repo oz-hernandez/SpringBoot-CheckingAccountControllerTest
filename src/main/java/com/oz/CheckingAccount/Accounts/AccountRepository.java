e134294adb80832befe05ddd0e491767a82a4ecb
Osmani-Josue Hernandez <osmani.j.hernandez@gmail.com>
2025-02-03 21:46:03 -0500

    find account by name

--- src/main/java/com/oz/CheckingAccount/Accounts/AccountRepository.java
+++ src/main/java/com/oz/CheckingAccount/Accounts/AccountRepository.java
@@ -2,5 +2,7 @@ package com.oz.CheckingAccount.Accounts;
 
 import org.springframework.data.repository.CrudRepository;
 
+
 public interface AccountRepository extends CrudRepository<Account, Long> {
+    Account findByName(String name);
 }
